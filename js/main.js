835266fb2767bae4470e1035028521c2f14c75a7
Andyyyy 3 <128613863+PatataPetit@users.noreply.github.com>
2023-05-23 09:09:54 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+//alert("Bienvenido a JavaScript");
+
+/*let nombre = prompt("Cual es tu nombre?");
+console.log(nombre)
+console.log(parseInt(nombre))
+console.log(typeof(nombre))*/
+
+let numerouno=prompt("Ingresa un numero");
+let numerodos=prompt("Ingresa otro numero");
+
+let resultadosuma=parseInt(numerouno) + parseInt(numerodos);
+let resultadoresta=parseInt(numerouno) - parseInt(numerodos);
+let resultadomulti=parseInt(numerouno) * parseInt(numerodos);
+let resultadodiv=parseInt(numerouno) / parseInt(numerodos);
+
+console.log(`El resultado de las Suma es:  ${resultadosuma}`);
+console.log(`El resultado de la Resta es:  ${resultadoresta}`);
+console.log(`El resultado de la Multiplicacion es:  ${resultadomulti}`);
+console.log(`El resultado de la Division es:  ${resultadodiv}`);
+
