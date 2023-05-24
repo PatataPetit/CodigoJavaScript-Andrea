4f24c9dc2d279815c19b35de4e634134916d0551
Andyyyy 3 <128613863+PatataPetit@users.noreply.github.com>
2023-05-24 09:07:32 -0600

    Numero Par

---
+++ main.js
@@ -0,0 +1,45 @@
+console.log("Hello World");
+//alert("Bienvenido a JavaScript");
+
+/*let nombre = prompt("Cual es tu nombre?");
+console.log(nombre)
+console.log(parseInt(nombre))
+console.log(typeof(nombre))*/
+
+/*let numerouno=prompt("Ingresa un numero");
+let numerodos=prompt("Ingresa otro numero");
+
+let resultadosuma=parseInt(numerouno) + parseInt(numerodos);
+let resultadoresta=parseInt(numerouno) - parseInt(numerodos);
+let resultadomulti=parseInt(numerouno) * parseInt(numerodos);
+let resultadodiv=parseInt(numerouno) / parseInt(numerodos);
+
+console.log(`El resultado de las Suma es:  ${resultadosuma}`);
+console.log(`El resultado de la Resta es:  ${resultadoresta}`);
+console.log(`El resultado de la Multiplicacion es:  ${resultadomulti}`);
+console.log(`El resultado de la Division es:  ${resultadodiv}`);*/
+
+//Dados dos numero indentificar cual es el numero mayor
+//Cual es menor o si son iguales
+
+/*let numeroUno = prompt ("Favor de ingresar un numero");
+let numeroDos = prompt ("Ingresa otro numero");
+
+if (numeroUno > numeroDos){
+console.log(`El numero ${numeroUno} es mayor que ${numeroDos}`);
+} else if (numeroDos > numeroUno){
+console.log(`El numero ${numeroDos} es mayor que ${numeroUno}`);
+}else{
+    console.log(`El numero ${numeroUno} es igual a ${numeroDos}`);
+}*/
+
+//Dado un numero identificar si es par o impar 
+
+let numero = prompt ("Ingresa un numero");
+
+if (numero%2==0){
+    console.log (`El numero ${numero} es par`);
+}else{
+    console.log(`El numero ${numero} es impar`);
+}
+
